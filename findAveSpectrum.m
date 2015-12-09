0bd0f61253e1d9fcc7e102539b4d0b82a64fc6d9
cowen314 <cowen314@gmail.com>
2015-12-08 21:50:41 -0500

    worked to further develop detector

---
+++ findAveSpectrum.m
@@ -0,0 +1,14 @@
+function [ averagedSpectrum, varianceVector ] = findAveSpectrum( ...
+    recordingDevice, aquisitionTime )
+%FINDAVESPECTURM  This function could be used as means of frequency domain
+%   noise calibration.
+%   
+%   Simply put, it would record background audio when no drones were
+%   present, then use the collected data to determine the background
+%   spectrum, and how much this spectrum tended to change (with
+%   varianceVector).
+
+    
+
+end
+
