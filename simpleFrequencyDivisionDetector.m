2bf425750ddc1a361756097f0a7f2508411c5e4e
cowen314 <cowen314@gmail.com>
2015-12-12 16:17:57 -0500

    started working on correlation peak detection

--- simpleFrequencyDivisionDetector.m
+++ simpleFrequencyDivisionDetector.m
@@ -15,6 +15,10 @@ NUM_SPECTRUM_SLICES = 20;
 SAMPLE_RATE_HZ = 44100;
 NUM_FRAMES_HELD = 4;
 WINDOW_SIZE = NUM_FRAMES_HELD*FRAME_SIZE;
+% for drones
+% COREL_KERNAL = [-55 -44 -34 -33 -34 -44 -55];
+
+
 
 har = dsp.AudioRecorder('NumChannels',NUM_CHANNELS,...
     'SamplesPerFrame',FRAME_SIZE,'SampleRate',SAMPLE_RATE_HZ);
@@ -26,29 +30,43 @@ disp('Speak into microphone now');
 tic;
 loopCounter = 0;
 timeseriesBuffer = zeros(WINDOW_SIZE,NUM_CHANNELS);
+% lastSpectrum will be used to see if corellation is useful
+% spectraBuffer = zeros(FRAME_SIZE,NUM_CHANNELS);
+lastSpectrum = zeros(WINDOW_SIZE/2,NUM_CHANNELS);
 [r,c] = size(timeseriesBuffer);
+% set chanNumber to 1 for now so that only channel one is active; this will
+% change when we need to collect data on several channels
 chanNumber = 1;
-while toc < 5
+while toc < 10
     singleAudioFrame = step(har);
     curFrame = singleAudioFrame(:,chanNumber);
-    % push new data into the buffer
+    % load the current frame into timeseriesBuffer and push the last frame
+    % out (each column holds NUM_FRAMES_HELD frames)
     timeseriesBuffer(:,chanNumber) = [curFrame; ...
         timeseriesBuffer(1:((NUM_FRAMES_HELD-1)*FRAME_SIZE),chanNumber)];
     % hit with a hamming window
     windowedData = timeseriesBuffer(:,chanNumber).*hamming(WINDOW_SIZE);
     % might want to play around with how fft is taken
     windowedDataF = 10*log10(abs(fft(windowedData.^2)));
+    % make the spectrum one-sided
     windowedDataF = windowedDataF(1:WINDOW_SIZE/2);
-    % live plots
-    % see: http://stackoverflow.com/questions/6681063/programming-in-matlab-how-to-process-in-real-time
-    % for better figure handling techniques
+    
+    xcorrelation = filter2(1/50*ones(50,1),windowedDataF);
+    
+    lastSpectrum(:,chanNumber) = windowedDataF;
+    
+    % live plots for better figure handling techniques see:
+    % http://stackoverflow.com/questions/6681063/programming-in-matlab-how-to-process-in-real-time
     if(mod(loopCounter,2)==0)
-        subplot(2,1,1)
+        subplot(3,1,1)
         plot(timeseriesBuffer(:,chanNumber))
         axis([0 WINDOW_SIZE -1 1])
-        subplot(2,1,2)
+        subplot(3,1,2)
         plot(windowedDataF)
         axis([1 WINDOW_SIZE/2 -60 0])
+        subplot(3,1,3)
+        plot(xcorrelation)
+        % axis([1 WINDOW_SIZE/2 -60 0])
         drawnow;
     end
 %     for chanNum = 1:NUM_CHANNELS
