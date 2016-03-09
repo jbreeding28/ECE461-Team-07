5712149d644261ba10b9cb1d87f46c229f685a71
boydlr <boydlr@rose-hulman.edu>
2016-03-08 20:31:48 -0500

    Remove Tolerances
    remove tolerances

--- Localization/localizer.m
+++ Localization/localizer.m
@@ -24,7 +24,7 @@ classdef Localizer
             A(I3) = [-50];
             [max4, I4] = max(A);
             A(I4) = [];
-            if (((max2*.95 <= max1) && (max1 <= max2*1.05)) && ((max3*.95 <= max1) && (max1 <= max3*1.05)) && ((max4*.95 <= max1) && (max1 <= max4*1.05)));
+            if (((max2*.95 <= max1) && (max1 <= max2*1.05)) && ((max3*.95 <= max1) && (max1 <= max3*1.05)) && ((max4*.95 <= max1) && (max1 <= max4*1.05)))
                 fprintf('Source is located directly above in the center\n');
             elseif (I1 == 1 && I2 == 3)
                 fprintf('There are two sources in quadrant 1 and 3\n');
@@ -35,44 +35,44 @@ classdef Localizer
             elseif (I1 == 4 && I2 == 2)
                 fprintf('There are two sources in quadrant 2 and 4\n');
             else
-                if (I1 == 1 && I2 == 2) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 1 && I2 == 2) && (max1 == max2)
                     fprintf('It is located N from the center\n');
-                elseif I1 == 1 && I2 == 2;
+                elseif I1 == 1 && I2 == 2
                     fprintf('It is located NNE from the center\n');
                 end
-                if (I1 == 1 && I2 == 4) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 1 && I2 == 4) && (max1 == max2)
                     fprintf('It is located E from the center\n');
-                elseif I1 == 1 && I2 == 4;
+                elseif I1 == 1 && I2 == 4
                     fprintf('It is located ENE from the center\n');
                 end
-                if (I1 == 2 && I2 == 1) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 2 && I2 == 1) && (max1 == max2)
                     fprintf('It is located N from the center\n');
-                elseif I1 == 2 && I2 == 1;
+                elseif I1 == 2 && I2 == 1
                     fprintf('It is located NNW from the center\n');
                 end
-                if (I1 == 2 && I2 == 3) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 2 && I2 == 3) && (max1 == max2)
                     fprintf('It is located W from the center\n');
-                elseif I1 == 2 && I2 == 3;
+                elseif I1 == 2 && I2 == 3
                     fprintf('It is located WNW from the center\n');
                 end
-                if (I1 == 3 && I2 == 2) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 3 && I2 == 2) && (max1 == max2)
                     fprintf('It is located W from the center\n');
-                elseif I1 == 3 && I2 == 2;
+                elseif I1 == 3 && I2 == 2
                     fprintf('It is located WSW from the center\n');
                 end
-                if (I1 == 3 && I2 == 4) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 3 && I2 == 4) && (max1 == max2)
                     fprintf('It is located S from the center\n');
-                elseif I1 == 3 && I2 == 4;
+                elseif I1 == 3 && I2 == 4
                     fprintf('It is located SSW from the center\n');
                 end
-                if (I1 == 4 && I2 == 3) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 4 && I2 == 3) && (max1 == max2)
                     fprintf('It is located S from the center\n');
-                elseif I1 == 4 && I2 == 3;
+                elseif I1 == 4 && I2 == 3
                     fprintf('It is located SSE from the center\n');
                 end
-                if (I1 == 4 && I2 == 1) && ((max2*.95 <= max1) && (max1 <= max2*1.05));
+                if (I1 == 4 && I2 == 1) && (max1 == max2)
                     fprintf('It is located E from the center\n');
-                elseif I1 == 4 && I2 == 1;
+                elseif I1 == 4 && I2 == 1
                     fprintf('It is located ESE from the center\n');
                 end
                 
